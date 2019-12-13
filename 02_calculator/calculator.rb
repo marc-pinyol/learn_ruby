d406e60a8d207c29e407b70687ab91e5d92ffffb
marc-pinyol <marc-pinyol@users.noreply.github.com>
2019-12-13 16:11:21 +0100

    finished exercies 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,8 +1,27 @@
 #write your code here
+def sum (nums)
+    return nums.inject(0, :+)
+end
+
 def add (num1, num2)
-    return num1 + num2
+    return sum([num1,num2])
 end
 
 def subtract(num1,num2)
     return num1-num2
+end
+
+def multiplies(nums)
+    return nums.inject(1, :*)
+end
+
+def power(base_value,power_value)
+    return base_value ** power_value 
+end
+
+def factorial(num)
+    if (num == 0)
+        return 1
+    end
+    return num.downto(1).inject(:*) 
 end
