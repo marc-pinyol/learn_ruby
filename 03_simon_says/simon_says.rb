770af0f51f2430c50eb0281c91a1c5e041385564
marc-pinyol <marc-pinyol@users.noreply.github.com>
2019-12-15 19:31:53 +0100

    Finished ex 02 and 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,26 @@
 #write your code here
+def echo (param)
+    return param
+end
+
+def shout(param)
+    return param.upcase
+end
+
+def repeat(param, repeats = 2)
+    return [param] * repeats * ' '
+end
+
+def start_of_word(param, position)
+    return param[0..position-1]
+end
+
+def first_word(param)
+    return param.split[0]
+end
+
+def titleize(param)
+    exc = ["and","over", "the"]
+    ret = param.split.map.with_index{|x,i| exc.include?(x) && i != 0  ? x : x.capitalize}*' '
+    return ret
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,39 @@
 #write your code here
+def translate(param)
+    param.split.map{|x|translate_word(x)}.join(' ')
+
+end
+
+def translate_word(param)
+    vowels = ["a","e","i","o","u"]
+    append = "ay"
+
+    if (vowels.include?(param[0]))
+        return param+append
+    
+    elsif(param.start_with?("qu"))
+        return param[2..-1]+param[0..1]+append
+    
+        # starts with 3 consonants
+    elsif (!vowels.include?(param[0]) && !vowels.include?(param[1]) && !vowels.include?(param[2]))
+        return param[3..-1]+param[0..2]+append
+    
+        # starts with consonants + "qu" (square)
+    elsif (param[1..-1].start_with?("qu"))
+        return  param[3..-1]+param[0..2]+append
+    
+        # starts with 2 consonants
+    elsif (!vowels.include?(param[0]) && !vowels.include?(param[1]))
+        return param[2..-1]+param[0..1]+append
+
+    # starts with 1 consonants
+    else
+        # aux =param[1..-1]
+        # return aux
+        # # if (aux.start_with?("qu"))
+        # #     return  "kk"#param[3..-1]+param[0..2]+append
+        # # else
+            return param[1..-1]+param[0]+append
+        # end
+    end
+end
